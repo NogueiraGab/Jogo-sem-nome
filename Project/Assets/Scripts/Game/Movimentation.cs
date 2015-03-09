d6e5ddc33128244a2fbf48c1e008de6ae17da9a7
Israel Nascimento <israelnascimento.nave@gmail.com>
2015-03-08 21:23:40 -0300

    Revert "Revert "Adicionado classe para movimentação""
    This reverts commit f55a933c74e8fbb6c16978c846add764eb98d5f2.

---
+++ Project/Assets/Scripts/Game/Movimentation.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum DIRECTION
+{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}
+public class Movimentation : MonoBehaviour 
+{
+
+    public static void Move(GameObject g, float speed = 0.05f, DIRECTION d = DIRECTION.DOWN)
+    {
+        switch (d)
+        {
+            case DIRECTION.DOWN:  g.transform.position += new Vector3 (0, 0, -speed);   break;
+
+            case DIRECTION.UP:    g.transform.position += new Vector3 (0, 0, speed);  break;
+
+            case DIRECTION.LEFT:  g.transform.position += new Vector3 (-speed, 0, 0);  break;
+
+            case DIRECTION.RIGHT: g.transform.position += new Vector3 (speed, 0, 0);   break;
+        }
+    }
+
+    public static void Jump(GameObject g, float force)
+    {
+        g.gameObject.rigidbody.AddForce(new Vector3(0, force, 0), ForceMode.Impulse);
+    }
+}
